c6c5716562716dcbc8e5c2faf69192f9351950b0
aimei60 <lizali26@outlook.com>
2025-11-26 13:37:34 +0000

    added the rest of my admin router functions

--- backend/application/routers/auth.admins.routes.js
+++ backend/application/routers/auth.admins.routes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import prisma from '../crud/auth.admins.js'  
 import bcrypt from "bcryptjs"  
-import { generateToken } from '../auth.js'
+import { generateToken, authRequired} from '../auth.js'
 import { toPublicAdmin, createAdmin, listAdmins, getAdminById, updateAdmin, deleteAdmin  } from '../crud/auth.admins.js'
 
 const router = express.Router()
@@ -50,6 +50,90 @@ router.post('/auth/login', async (req, res) => {
   } catch (error) {
     console.error(error)
     res.status(500).json({ error: 'Failed to log in' })
+  }
+})
+
+//logged in admin create admin user
+router.post('/admins', authRequired, async (req, res) => {
+  try {
+    const currentAdmin = req.user //stores details of current admin or superadmin
+    const { email, password, role } = req.body //details of new admin
+
+    const newAdmin = await createAdmin(currentAdmin, { email, password, role })
+    res.status(201).json(newAdmin)
+  } catch (error) {
+    res.status(400).json({ error: error.message || 'Failed to create admin' })
+  }
+})
+
+//router function to list admins
+router.get('/admins', authRequired, async (req, res) => {
+  try {
+    const currentAdmin = req.user
+    const adminList = await listAdmins(currentAdmin)
+
+    res.status(200).json(adminList)
+  } catch (error) {
+    res.status(404).json({ error: error.message || 'Failed to return list of admins' })
+  }
+})
+
+//router to get admin by id
+router.get('/admins/:id', authRequired, async (req, res) => {
+  try {
+    const currentAdmin = req.user
+    const { id } = req.params     
+
+    const adminById = await getAdminById(currentAdmin, id)
+
+    if (!adminById) {
+      return res.status(404).json({ error: 'Admin not found' })
+    }
+
+    res.status(200).json(adminById)
+  } catch (error) {
+    if (error.message === 'Not allowed') {
+      return res.status(403).json({ error: 'Not allowed'})
+    }
+    console.error(error)
+    res.status(500).json({ error: error.message || 'Failed to get admin' })
+  }
+})
+
+//router function to update admin
+router.put('/admins/:id', authRequired, async (req, res) => {
+  try {
+    const currentAdmin = req.user
+    const { id } = req.params    
+    const { email, password, role } = req.body 
+
+    const updatesAdmin = await updateAdmin(currentAdmin, id, { email, password, role })
+
+    if (!updatesAdmin) {
+      return res.status(404).json({ error: 'Admin not found' })
+    }
+
+    res.json(updatesAdmin)
+  } catch (error) {
+    res.status(400).json({ error: error.message || 'Failed to update admin' })
+  }
+})
+
+// router function to delete admin
+router.delete('/admins/:id', authRequired, async (req, res) => {
+  try {
+    const currentAdmin = req.user
+    const { id } = req.params    
+
+    const delAdmin = await deleteAdmin(currentAdmin, id)
+
+    if (!delAdmin) {
+      return res.status(404).json({ error: 'Admin not found' })
+    }
+
+    res.json(delAdmin)
+  } catch (error) {
+    res.status(400).json({ error: error.message || 'Failed to delete admin' })
   }
 })
 
